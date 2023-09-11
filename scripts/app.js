199fc31190d5ce3d30a7e1fee9cbbc3ae3711c47
Soh Wen Ming <wenming.soh@temus.com>
2023-09-11 13:29:55 +0800

    committed from work

--- scripts/app.js
+++ scripts/app.js
@@ -6,11 +6,10 @@ const operations = ["BALANCE_SHEET", "TIME_SERIES_DAILY", "OVERVIEW"];
 //main api call - called in subsequent async functions with different params
 async function getData(operation, symbol, apiKey) {
   try {
-    const result = await axios.get(`https://www.alphavantage.co/query?function=${operation}&symbol=${symbol}&apikey=${apiKey}`);
-    console.log("result: ", result);
-    console.log("result.data: ", result.data);
+    const result = await axios.get(
+      `https://www.alphavantage.co/query?function=${operation}&symbol=${symbol}&apikey=${apiKey}`
+    );
     let data = result.data;
-    console.log("data:", data);
     return data;
   } catch {
     (error) => {
@@ -18,7 +17,6 @@ async function getData(operation, symbol, apiKey) {
     };
   }
 }
-
 //used to get Basic Bank Information - Name and Description. Returns an object
 async function getBankInformation(symbol) {
   try {
@@ -38,7 +36,6 @@ async function getBankInformation(symbol) {
     };
   }
 }
-
 //used to pull total balance sheet information, gets all annual and quarterly reports
 async function getBalanceSheet(symbol) {
   const data = await getData(operations[0], symbol, apiKey);
@@ -57,62 +54,85 @@ async function getQuarterlyReport(symbol) {
 }
 
 async function calcTBV(ticker) {
-  let data = await getQuarterlyReport(ticker);
-  let qtrRepList = [];
-  data.forEach((report) => {
-    qtrRepList.push(report);
-  });
-  qtrRepList.forEach((report) => {
-    getTBV(report);
-  });
+  try {
+    let data = await getQuarterlyReport(ticker);
+    console.log(data);
+    let qtrRepList = [];
+    let tbvList = [];
+    data.forEach((report) => {
+      qtrRepList.push(report);
+    });
+    console.log("qtrRep: ", qtrRepList);
+    qtrRepList.forEach((report) => {
+      tbvList.push(getTBV(report));
+      console.log("pushed");
+    });
+    return tbvList;
+  } catch {
+    (error) => {
+      console.log(error);
+    };
+  }
 }
 
 function getTBV(report) {
+  let tbv = {};
   const fiscalDateEnding = report.fiscalDateEnding;
   const totalAssets = report.totalAssets;
   const goodWill = report.goodwill;
   const intangibles = report.intangibleAssets;
   const liabilities = report.totalLiabilities;
   const shares = report.commonStockSharesOutstanding;
-  const tbvps = ((totalAssets - goodWill - intangibles - liabilities) / shares).toFixed(2);
+  const tbvps = (
+    (totalAssets - goodWill - intangibles - liabilities) /
+    shares
+  ).toFixed(2);
+  tbv["fiscal date ending"] = fiscalDateEnding;
+  tbv["tbv"] = tbvps;
+  return tbv;
 }
-// function getLatestDate(dateArray) {
-//   let latestDate = dateArray[0];
-//   for (let i = 0; i < dateArray.length; i++) {
-//     if (dateArray[i] > latestDate) {
-//       latestDate = dateArray[i];
-//     }
-//   }
-//   return latestDate;
-// }
 
-// function getStringFromDate(date) {
-//   const year = date.getFullYear();
-//   const month = String(date.getMonth() + 1).padStart(2, "0");
-//   const day = String(date.getDate()).padStart(2, "0");
-//   const formattedDate = `${year}-${month}-${day}`;
-//   return formattedDate;
-// }
+async function getDayClose(symbol) {
+  try {
+    const result = await axios.get(
+      `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${apiKey}`
+    );
+    console.log("result", result.data);
+    const timeSeries = result.data["Time Series (Daily)"];
+    console.log("timeseries", timeSeries);
+    const dateText = Object.keys(timeSeries);
+    console.log("dateText", dateText);
+    let dates = dateText.map((date) => {
+      return new Date(date);
+    });
+    const latestDate = getLatestDate(dates);
+    latestDateString = getStringFromDate(latestDate);
+    console.log(latestDateString);
+    console.log(timeSeries[latestDateString]["4. close"]);
+  } catch {
+    (error) => {
+      console.log(error);
+    };
+  }
+}
 
-// async function getdayClose(symbol) {
-//   try {
-//     const result = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${apiKey}`);
-//     console.log(result.data);
-//     const timeSeries = result.data["Time Series (Daily)"];
-//     const dateText = Object.keys(timeSeries);
-//     let dates = dateText.map((date) => {
-//       return new Date(date);
-//     });
-//     const latestDate = getLatestDate(dates);
-//     latestDateString = getStringFromDate(latestDate);
-//     console.log(latestDateString);
-//     console.log(timeSeries[latestDateString]["4. close"]);
-//   } catch {
-//     (error) => {
-//       console.log(error);
-//     };
-//   }
-// }
+function getLatestDate(dateArray) {
+  let latestDate = dateArray[0];
+  for (let i = 0; i < dateArray.length; i++) {
+    if (dateArray[i] > latestDate) {
+      latestDate = dateArray[i];
+    }
+  }
+  return latestDate;
+}
+
+function getStringFromDate(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const formattedDate = `${year}-${month}-${day}`;
+  return formattedDate;
+}
 
 //   let information = {
 //     "Fiscal Date Ending": fiscalDateEnding,
@@ -126,3 +146,5 @@ function getTBV(report) {
 // }
 
 // so i want an which will be passed two objects, name and description, and then
+
+///deal later///
